c28f92ca45a2dd86b9a21d605b2e62644d89d363
MSI\Wu <zhangwu@sheridancollege.ca>
2020-02-08 11:29:58 -0500

    Building LevelSpawner

---
+++ Assets/_Hasteland/Scripts/LevelWu/LevelController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public Transform spawnPos, despawnPos;
+
+    public GameObject[] spawnReference;
+
+    private PoolManager poolManager;
+
+    private bool isLevelFinished;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        poolManager = PoolManager.Instance;
+
+        StartCoroutine(SpawnIntervals());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnIntervals()
+    {
+        while (!isLevelFinished)
+        {
+
+            float duration = 3f;
+            float totalTime = 0f;
+
+            while (totalTime <= duration)
+            {
+                totalTime += Time.deltaTime;
+                //do nothing;
+                yield return null;
+            }
+            //Spawn tile here
+            spawnMapSections();
+            //Reset Spawntimer;
+            totalTime = 0;
+        }
+    }
+
+    void spawnMapSections()
+    {
+        var terrainName = spawnReference[Random.Range(0, spawnReference.Length)].ToString();
+
+        poolManager.SpawnFromPool(terrainName, spawnPos.transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/_Hasteland/Scripts/LevelWu/TerrainMover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainMover : MonoBehaviour
+{
+
+    public int terrainSpd;
+    public bool isLevelComplete;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(moveTiles(terrainSpd));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    IEnumerator moveTiles(int terrainSpd)
+    {
+        while (true)
+        {
+            transform.localPosition += new Vector3(-1, 0, 0) * terrainSpd * Time.deltaTime;
+
+            yield return null;
+        }
+    }
+}
