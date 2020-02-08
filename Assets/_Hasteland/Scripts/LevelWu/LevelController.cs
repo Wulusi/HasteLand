ec7e5ea5a8256f11bc49965f09ab472c30dfc0dd
MSI\Wu <zhangwu@sheridancollege.ca>
2020-02-08 15:22:21 -0500

    tis a sad day when upate is more consistent than fixedupdate

--- Assets/_Hasteland/Scripts/LevelWu/LevelController.cs
+++ Assets/_Hasteland/Scripts/LevelWu/LevelController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class LevelController : MonoBehaviour
 {
@@ -12,12 +13,19 @@ public class LevelController : MonoBehaviour
 
     private PoolManager poolManager;
 
+    public float terrainCount;
+
     private bool isLevelFinished;
+
+    private int randomInt, lastInt;
 
     // Start is called before the first frame update
     void Start()
     {
         poolManager = PoolManager.Instance;
+        terrainCount = 0;
+        //Assign 0 by default
+        lastInt = 0;
 
         //StartCoroutine(SpawnIntervals());
     }
@@ -25,64 +33,57 @@ public class LevelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
+        SpawnTerrain();
     }
 
     private void FixedUpdate()
     {
-        SpawnTerrain();
     }
 
     private void SpawnTerrain()
     {
-        float totalTime = Time.time;
-        float duration = 4f;
+        float totalTime = Time.frameCount;
+        float duration = 240f;
 
         //Spawn a new section of the map every 4 seconds
-        if(totalTime % duration == 0)
+        if (Mathf.Repeat(totalTime, duration) == 0)
         {
+            Debug.Log("Before Spawn Count" + terrainCount);
             spawnMapSections();
         }
     }
 
-    IEnumerator SpawnIntervals()
-    {
-        float totalTime = Time.time;
+    //IEnumerator SpawnIntervals()
+    //{
+    //    float totalTime = Time.time;
 
-        while (!isLevelFinished)
-        {
-            while (Mathf.Repeat(totalTime, 4) == 0)
-            {
-                Debug.Log("Spawning Tile " + totalTime);
-                //Not sure why this is giving null ref
-                spawnMapSections();
-                yield return null;
-            }
-        }
-    }
+    //    while (!isLevelFinished)
+    //    {
+    //        while (Mathf.Repeat(totalTime, 4) == 0)
+    //        {
+    //            Debug.Log("Spawning Tile " + totalTime);
+         
+    //            //Not sure why this is giving null ref
+    //            spawnMapSections();
+    //            yield return null;
+    //        }
+    //    }
+    //}
 
     void spawnMapSections()
     {
-        var terrain = spawnReference[Random.Range(0, spawnReference.Length)];
+        terrainCount++;
 
-        bool isFirstSpawn = false;
+        //randomInt = Random.Range(0, spawnReference.Length);
+        //GameObject currentTerrain =
+        //    poolManager.SpawnFromPool(spawnReference[randomInt].name, spawnPos.transform.position, Quaternion.identity);
 
-        if (!isFirstSpawn)
-        {
-            Debug.Log("terrain name is " + terrain);
+        var rnd = new System.Random();
+        var randomNumbers = Enumerable.Range(0, spawnReference.Length).OrderBy(x => rnd.Next()).Take(6).ToList();
 
-            poolManager.SpawnFromPool(terrain.name, spawnPos.transform.position, Quaternion.identity);
-        } else
-        {
-            GameObject lastTerrain = terrain;
-            if(terrain != lastTerrain)
-            {
-                poolManager.SpawnFromPool(terrain.name, spawnPos.transform.position, Quaternion.identity);
-            } else
-            {
-                poolManager.SpawnFromPool(terrain.name, spawnPos.transform.position, Quaternion.identity);
-            }
+        GameObject currentTerrain =
+            poolManager.SpawnFromPool(spawnReference[randomNumbers[0]].name, spawnPos.transform.position, Quaternion.identity);
 
-        }
+        Debug.Log("Spawned Terrain " + terrainCount + " " + currentTerrain);
     }
 }
