df9a69f8a98cfa356cc4c15cc2df3fd314962959
TylerTam <46612488+TylerTam@users.noreply.github.com>
2020-03-25 17:01:54 -0400

    Added environment Pieces

--- Assets/_Hasteland/Scripts/LevelWu/TerrainMover.cs
+++ Assets/_Hasteland/Scripts/LevelWu/TerrainMover.cs
@@ -8,6 +8,14 @@ public class TerrainMover : MonoBehaviour, PooledObjInterface
     public int terrainSpd;
     public bool isLevelComplete;
 
+    [Range(0,5)]
+    public int m_maxItems;
+    public List<Transform> m_objectSpawnPositions;
+    public List<GameObject> m_spawnablePrefabs;
+    private ObjectPooler m_pooler;
+
+    private List<GameObject> m_environmentObjects = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +25,7 @@ public class TerrainMover : MonoBehaviour, PooledObjInterface
         }
     }
 
+    
     IEnumerator moveTiles(int terrainSpd)
     {
         while (true)
@@ -37,8 +46,35 @@ public class TerrainMover : MonoBehaviour, PooledObjInterface
         GameHub.LevelController.currentTerrainList.Add(this.gameObject);
     }
 
+    private void OnEnable()
+    {
+        CreateRandomizedTerrain();
+    }
+
+    private void CreateRandomizedTerrain()
+    {
+        if(m_pooler == null)
+        {
+            m_pooler = ObjectPooler.instance;
+        }
+
+        int randomAmount = Random.Range(0, m_maxItems);
+        List<Transform> possibleSpawns = new List<Transform>(m_objectSpawnPositions);
+
+        for (int i = 0; i < randomAmount; i++)
+        {
+            int randomSpawn = Random.Range(0, possibleSpawns.Count);
+            
+            GameObject newPiece = m_pooler.NewObject(m_spawnablePrefabs[Random.Range(0, m_spawnablePrefabs.Count)], possibleSpawns[randomSpawn].position, Quaternion.Euler(0, Random.Range(0, 360), 0));
+            m_environmentObjects.Add(newPiece);
+            newPiece.transform.parent = transform;
+            m_objectSpawnPositions.RemoveAt(randomSpawn);
+        }
+    }
     public void OnDisable()
     {
+
+        ClearEnvironmentPieces();
         StopCoroutine(moveTiles(terrainSpd));
 
         if (isStartingTiles)
@@ -52,5 +88,14 @@ public class TerrainMover : MonoBehaviour, PooledObjInterface
                 GameHub.LevelController.currentTerrainList.Remove(this.gameObject);
             }
         }
+    }
+
+    private void ClearEnvironmentPieces()
+    {
+        foreach(GameObject piece in m_environmentObjects)
+        {
+            m_pooler.ReturnToPool(piece);
+        }
+        m_environmentObjects.Clear();
     }
 }
