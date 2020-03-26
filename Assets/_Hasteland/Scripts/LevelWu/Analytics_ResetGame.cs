9edd08aac1f55cf352a565c8cb9856f563b3074a
MSI\Wu <zhangwu@sheridancollege.ca>
2020-03-25 21:04:55 -0400

    Minor bug fixes

---
+++ Assets/_Hasteland/Scripts/LevelWu/Analytics_ResetGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Analytics_ResetGame : MonoBehaviour
+{
+    public void ResetGame()
+    {
+        GameHub.TrackEventsContinuous.RestartGame();
+    }
+}
